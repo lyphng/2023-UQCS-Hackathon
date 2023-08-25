4193db6f85042beaca5969d062cc32c5332201f4
unknown <Peter1031734590@gmail.com>
2023-08-25 21:44:58 +1000

    Added functionality to read line by line

--- main.py
+++ main.py
@@ -8,14 +8,36 @@ def check_prerequisites(target, courses):
             #prerequisites = 
     return prerequisites
 
-pdf_file = open('example.pdf', 'rb')
+def read_pdf_page(file_name, page_number):
 
+    pdf_file = open(file_name, 'rb')
+
+    reader = PdfReader(pdf_file)
+    
+    page = reader.pages[page_number]
+    
+    text = page.extract_text()
+    pdf_file.close()
+
+    return text
+
+#Reading line by line so attributes can be properly filtered
+pdf_file = open("example.pdf",'rb')
+    
 reader = PdfReader(pdf_file)
-  
-page = reader.pages[0]
-  
-text = page.extract_text()
+    
+pages = len(reader.pages)
 
-print(text)
+for i in range(pages):
+
+        page = reader.pages[i]
+
+        print("Page No: ", i + 1)
+
+        text = page.extract_text().split("\n")
+        for i in range(len(text)):
+
+                print("Line Number : {i} - {text} \n\n".format(i = i, text = text[i]))
+        print()
 
 pdf_file.close()
