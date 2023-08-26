822887f1f6ab3b5625559811a0996f4bea5bfb7f
unknown <Peter1031734590@gmail.com>
2023-08-26 17:50:08 +1000

    uploading extension files

---
+++ extension/scripts/content_scripts.js
@@ -0,0 +1,7 @@
+
+const courseList = document.querySelector("div.course-org-list");
+console.log(courseList)
+if(courseList) {
+    console.log(courseList)
+    alert("FOUND YA BITCH")
+}
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PyPDF2 import PdfReader
+import json
 
 def check_prerequisites(target, courses):
     prerequisites = []
@@ -21,23 +22,32 @@ def read_pdf_page(file_name, page_number):
 
     return text
 
+
+json_file = open('course_data.json', "r")
+data = json.load(json_file)
+for line in data['courses']:
+    if line['course'] == 'CSSE2310':
+        print(line)
+json_file.close()
+
 #Reading line by line so attributes can be properly filtered
-pdf_file = open("example.pdf",'rb')
+# pdf_file = open("example.pdf",'rb')
     
-reader = PdfReader(pdf_file)
+# reader = PdfReader(pdf_file)
     
-pages = len(reader.pages)
+# pages = len(reader.pages)
 
-for i in range(pages):
+# for i in range(pages):
 
-        page = reader.pages[i]
+#         page = reader.pages[i]
 
-        print("Page No: ", i + 1)
+#         print("Page No: ", i + 1)
 
-        text = page.extract_text().split("\n")
-        for i in range(len(text)):
+#         text = page.extract_text().split("\n")
+#         for i in range(len(text)):
+#             print("Line Number : {i} - {text} \n\n".format(i = i, text = text[i].split("     ")))
+#                 #print("Line Number : {i} - {text} \n\n".format(i = i, text = text[i]))
+#         print()
 
-                print("Line Number : {i} - {text} \n\n".format(i = i, text = text[i]))
-        print()
 
-pdf_file.close()
+#pdf_file.close()
